5ad1169a0a4013c584d3f5b8c1382795480cdfa8
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2020-05-21 09:10:23 -0500

    Update to Maps V3 Beta and to latest other dependencies

--- app/src/main/java/com/bubenheimer/mapleak/MainActivity.java
+++ app/src/main/java/com/bubenheimer/mapleak/MainActivity.java
@@ -1,7 +1,8 @@
 package com.bubenheimer.mapleak;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public final class MainActivity extends AppCompatActivity {
     @Override
