12064fb7125a7b5e731a0897eb6038549e850500
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2020-07-13 23:25:41 -0500

    Update to Maps V3 Beta 3.1.0, change AGP to 4.0.0 for easier reproducing, refine sample code

--- app/src/main/java/com/bubenheimer/mapleak/MainActivity.java
+++ app/src/main/java/com/bubenheimer/mapleak/MainActivity.java
@@ -1,10 +1,17 @@
 package com.bubenheimer.mapleak;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public final class MainActivity extends AppCompatActivity {
+    private static final AtomicInteger INSTANCE_COUNTER = new AtomicInteger(0);
+
+    private final int instanceNo = INSTANCE_COUNTER.incrementAndGet();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +21,11 @@ public final class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             getSupportFragmentManager().findFragmentById(R.id.fragment).setRetainInstance(true);
         }
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        Log.d("MainActivity", "Finalizing instance " + instanceNo);
+        super.finalize();
     }
 }
